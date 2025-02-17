41b39615cb133e0afd629649e8a79c2ea25bf7fd
Dev khatri <khatridev318@gmail.com>
2025-02-17 17:00:20 +0500

    Create 2 SignUp and a login frame

---
+++ src/main/java/com/mycompany/bankmanagement/BankManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bankmanagement;
+
+/**
+ *
+ * @author Dev khatri
+ */
+public class BankManagement {
+
+    public static void main(String[] args) {
+        new SignUp().setVisible(true);
+    }
+}
---
+++ src/main/java/model/account.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dev khatri
+ */
+public class account {
+  private int userId;
+  private String name;
+  private String email;
+  private String phoneNumber;
+  private String accountNumber;
+  private int pin;
+}
